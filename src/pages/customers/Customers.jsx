577847a49d8c2dd88fad9e523c504eb732a0d310
Vivek Prasad <flashtech007@gmail.com>
2024-02-28 20:06:53 +0530

    feat - added Customers and Statistics Page

---
+++ src/pages/customers/Customers.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import TopPieCards from '../../components/dashboard/dashboard-top/TopPieCards'
+import PrimeReactTable from '../../components/dashboard/dashboard-bottom/PrimeReactTable'
+
+
+const Customers = () => {
+
+
+  return (
+    <div className='flex flex-col gap-4 w-full py-3'>
+        <TopPieCards/>
+
+
+        <div className='flex flex-col md:flex-row gap-4 w-full py-6'>
+            <PrimeReactTable/>
+        </div>
+
+      
+        
+    </div>
+  )
+}
+
+export default Customers
