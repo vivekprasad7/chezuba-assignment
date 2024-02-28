bf2bd4a79d9e5c15de5c74f2bf4deda1b1a5a639
Vivek Prasad <flashtech007@gmail.com>
2024-02-28 19:17:47 +0530

    feat - added routes and shared component pages -Shared - -Layout - -Sidebar -Pages --Dashboard --Products

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Layout from './components/shared/Layout'
+import Dashboard from './pages/dashboard/Dashboard'
+import Products from './pages/products/Products'
 
 function App() {
 
@@ -7,6 +11,16 @@ function App() {
     
       <div>
           <h1 className='text-3xl text-red-400'>Hello</h1>
+
+          <Routes>
+          <Route path="/" element={<Layout/>}>
+              <Route index element={<Dashboard/>}/>
+              <Route path="products" element={<Products/>}/>
+          </Route>
+        </Routes>
+
+
+
       </div>
      
     
---
+++ src/components/shared/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Sidebar from './Sidebar'
+
+const Layout = () => {
+  return (
+    <div className='flex w-[100%] '>
+        hello
+        <div><Sidebar/></div>
+        <div className='bg-slate-50 flex flex-col flex-1 h-full'>
+
+        {/* <div>Header</div> */}
+        <div className='flex-1 p-4 min-h-0 '>{<Outlet/>}</div>
+        </div>
+    </div>
+  )
+}
+
+export default Layout
